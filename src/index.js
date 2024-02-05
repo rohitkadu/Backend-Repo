0444a3abdf8c127339629109e2f9017fa788619e
Rohit Kadu <rohitkadu3102@gmail.com>
2024-02-05 23:22:51 +0530

    Standard Database Connection Handling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ dotenv.config({
 
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at ${process.env.PORT}`)
+    })
+} )
+.catch( (err) => {
+    console.log("MongoDB Connection Failed!! Async Function Responce Error = ")
+} )
 
 
 
